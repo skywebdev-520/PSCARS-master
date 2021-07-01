920a7c2a8002abd4a5ba5d21acb99ac8d4940321
sky <sky@gmail.com>
2021-07-01 08:09:47 -0300

    change

--- app/Controllers/Http/CheckoutController.js
+++ app/Controllers/Http/CheckoutController.js
@@ -35,16 +35,18 @@ class CheckoutController {
         let carJSN = car.toJSON()
         let bookings = await Booking.query().where("car_id",params.carid).select("id","checkout","checkin").where("status","NOT LIKE","ABGESCHLOSSEN").where("status","NOT LIKE","STORNIERT").where("checkout", ">", moment().format("YYYY-MM-DD")).fetch()
         let blocked = await Blocked.query().where("car_id",params.carid).select("id","checkout","checkin").where("checkout", ">=", moment().format("YYYY-MM-DD")).fetch()        
-        var todate=  blocked.toJSON()[0]['checkin']
-        let edDate = new Date(todate)
-        todate =  blocked.toJSON()[0]['checkout']
-        let stdate = new Date(todate)
-        let disabled = []
-        let itDate = stdate
-        while(itDate <= edDate) {         
-            let temp = moment(itDate,"YYYY-MM-DD").add(1, 'days')
-            disabled.push(moment(temp).format("YYYY-MM-DD"))
-            itDate.setDate(itDate.getDate() + 1)
+        let disabled = []        
+        if(blocked.toJSON()[0] != undefined){
+            var todate=  blocked.toJSON()[0]['checkin']
+            let edDate = new Date(todate)
+            todate =  blocked.toJSON()[0]['checkout']
+            let stdate = new Date(todate)            
+            let itDate = stdate
+            while(itDate <= edDate) {         
+                let temp = moment(itDate,"YYYY-MM-DD").add(1, 'days')
+                disabled.push(moment(temp).format("YYYY-MM-DD"))
+                itDate.setDate(itDate.getDate() + 1)
+            }        
         }        
         let bkings = bookings.toJSON().concat(blocked.toJSON())
         // let disabled=[]
